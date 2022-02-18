f20dcb37a80f55a3753227fc4cc3ee947cf4c296
fily1212 <fily1212@gmail.com>
2022-02-18 10:21:55 +0100

    Esempi di metodo

---
+++ Lezione3/MetodiEsempi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MetodiEsempi {
+    
+    public static int somma(int a, int b){
+        // int c = a + b;
+        // return c;
+        // VIETATO: int a = System.out.println(a); 
+        return a + b; 
+    }
+    public static void main(String[] args) { 
+
+        int num = somma(1,2);
+        System.out.println(num);
+    } 
+ 
+
+}
