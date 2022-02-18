05e917c2ca1e9b657eb99e6b4af2b88a6a5bec5d
fily1212 <fily1212@gmail.com>
2022-02-18 11:19:35 +0100

    Esempio Stack

---
+++ Lezione3/EsempioStack.java
@@ -0,0 +1,15 @@
+public class EsempioStack {
+    public static int doppio(int n){
+        return n*2;
+    }
+    public static int triplo(int n){
+        return n*3;    
+    }
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 5;
+        a = b - 1 ;
+        a = doppio(a);
+        int c = triplo(b);
+    }
+}
--- Lezione3/MetodiEsempi.java
+++ Lezione3/MetodiEsempi.java
@@ -11,17 +11,20 @@ public class MetodiEsempi {
 
      //   int num = somma(1,2);
      //   System.out.println(num);
-
-        int[] a = {1,2,3,4,5,6,7};
-
+       // int[] a = {1,2,3,4,5,6,7};
        // Util.stampa(a);
-
-        a[1] = 4;
-
+      //  a[1] = 4;
       //  Util.stampa(a);
+ 
 
+        // riempire un array, ordinarlo in modo crescente, poi stamparlo
+
+        
         int[] b = Util.creaArray();
+        Util.ordinaArray(b);
         Util.stampa(b);
+
+
     } 
  
 
