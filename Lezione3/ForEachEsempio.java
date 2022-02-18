46846474eaf0da96288fc91f549272cda96ac222
fily1212 <fily1212@gmail.com>
2022-02-18 10:21:48 +0100

    For each

---
+++ Lezione3/ForEachEsempio.java
@@ -0,0 +1,15 @@
+public class ForEachEsempio {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+
+        for (int elem : array) {
+            System.out.println(elem);
+        }
+        /*
+        sintassi: 
+        for([tipo] [nomeVariabileElemento] : [nomeArray]){
+             istruzioni
+        }
+        */
+    }
+}
