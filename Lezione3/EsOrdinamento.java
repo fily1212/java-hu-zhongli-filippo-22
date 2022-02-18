6eebc4587b8026249d4dd1028f3625c5f7675a1f
fily1212 <fily1212@gmail.com>
2022-02-18 12:47:19 +0100

    es swap

--- Lezione3/EsOrdinamento.java
+++ Lezione3/EsOrdinamento.java
@@ -1,11 +1,27 @@
 public class EsOrdinamento {
     // fare un metodo che ritorna la posizione del numero massimo in un array
     public static int posMax(int[] a){
+       int posMax = 0;
+       for(int i=1; i<a.length; i++){
+           if (a[i] > a[posMax])
+            posMax = i;
+       } 
+       return posMax;
+    }
 
+    // metodo swap che dato un array e due posizioni, scambi i valori all'interno
+    public static void swap(int[] array, int pos1, int pos2){
+        
     }
 
     public static void main(String[] args) {
-        int[] a = {1,6,9,-12};
+        int[] a = {3,-6,9,-12};
+        //         0 1  2  3
+        // posMax: 2 
+        // i : 2
+
+        //{1}
+        //pos: 0
         System.out.println(posMax(a));
         // deve stampare 2
     }
