0f71d4f37cd66ba0cd4535f87755557d66e02583
fily1212 <fily1212@gmail.com>
2022-02-18 13:13:03 +0100

    StampaPari

--- Lezione3/RicorsioneEsempio.java
+++ Lezione3/RicorsioneEsempio.java
@@ -9,5 +9,22 @@ public class RicorsioneEsempio {
     }
     public static void main(String[] args) {
         metodoRicorsivo(3);
+
+        stampaPari(5);
     }
+
+    public static void stampaPari(int n){
+    // Esercizio 2: (scrivi un metodo ricorsivo che stampi i numeri pari da 0 a n)
+    // esempio: input: 5, stampa: 0 2 4 
+
+    }
+
+
 }
+
+/* Ricorsione: come fare? Consiglio: partire sempre da caso base
+ e poi definire il passo induttivo. 
+Cos'è il caso base? E' il caso dove viene fermata la ricorsione
+Cosa sono i passi induttivi? Sono tutti gli altri casi, in cui la ricorsione continua.
+ 
+*/
