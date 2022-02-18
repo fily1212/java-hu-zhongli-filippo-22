b022177f1f42b19c089f849f3734f3c3c5f5777f
fily1212 <fily1212@gmail.com>
2022-02-18 10:22:07 +0100

    Creazione classe util con metodo stampa

---
+++ Lezione3/Util.java
@@ -0,0 +1,20 @@
+public class Util {
+    
+    // metodo che stampa un array di interi
+    public static void stampa(int [] pippo){
+        System.out.print("["); 
+        for (int i=0; i<pippo.length;i++){
+            System.out.print(pippo[i]);
+            if(i!=pippo.length-1){
+                System.out.print(", ");
+            } 
+        }
+        System.out.print("]");
+    }  
+}
+
+
+    
+    
+    
+     
