89bf5b5caf40ca634f03bb049d23e7e5b11a4b43
Zhongli Filippo Hu <zhonglifilippo.hu@unito.it>
2022-02-25 12:56:48 +0100

    forme

--- Lezione4/prova-intellij/src/com/engim/lezione5/BottigliaConTappo.java
+++ Lezione4/prova-intellij/src/com/engim/lezione5/BottigliaConTappo.java
@@ -8,10 +8,27 @@ public class BottigliaConTappo extends Bottiglia{
         aperta = true;
     }
 
+    public BottigliaConTappo(int capacita, boolean aperta){
+        super(capacita);
+        this.aperta = aperta;
+    }
+
     public void apri(){
         aperta = true;
     }
     public void chiudi(){
         aperta = false;
+    }
+
+    @Override
+    public void riempi(int q) {
+        if(aperta)
+            super.riempi(q);
+    }
+
+    @Override
+    public void svuota(int q) {
+        if(aperta)
+            super.svuota(q);
     }
 }
