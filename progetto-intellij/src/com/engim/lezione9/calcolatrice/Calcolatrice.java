e24c30a489eb1c90fb76c18bbd2bbc9e6c9a00b4
Zhongli Filippo Hu <zhonglifilippo.hu@unito.it>
2022-03-08 12:05:52 +0100

    calcolatrice esercizio

--- progetto-intellij/src/com/engim/lezione9/calcolatrice/Calcolatrice.java
+++ progetto-intellij/src/com/engim/lezione9/calcolatrice/Calcolatrice.java
@@ -1,19 +1,55 @@
 package com.engim.lezione9.calcolatrice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calcolatrice {
+    static ArrayList<Double> pila;
+
+    public static double pop(){
+        double n = pila.get(pila.size()-1);
+        pila.remove(pila.size()-1);
+        return n;
+    }
+
+    public static void addizione(){
+        if(pila.size() >= 2){
+            double n2 = pop();
+            double n1 = pop();
+            aggiuntaNum(n1+n2);
+        }
+    }
+
+    public static void sottrazione(){
+
+    }
+
+    public static void moltiplicazione(){
+
+    }
+
+    public static void divisione(){
+
+    }
+    public static void aggiuntaNum(double num){
+        pila.add(num);
+    }
+
+
+
     public static void main(String[] args) {
+        pila = new ArrayList<>();
         System.out.println("Benvenuto nella calcolatrice. Inserisci un'espressione in notazione polacca inversa.");
         Scanner tastiera = new Scanner(System.in);
-        while(tastiera.hasNext()){
+        boolean finito = false;
+        while(!finito){
             String inserito = tastiera.next();
             switch (inserito){
                 case "+":
-                    somma();
+                    addizione();
                     break;
                 case "-":
-                    differenza();
+                    sottrazione();
                     break;
                 case "*":
                     moltiplicazione();
@@ -21,8 +57,14 @@ public class Calcolatrice {
                 case "/":
                     divisione();
                     break;
+                case "=":
+                    System.out.println(pila);
+                    break;
+                case "!":
+                    finito=true;
+                    break;
                 default:
-                    aggiuntaNum();
+                    aggiuntaNum(Double.parseDouble(inserito));
             }
         }
     }
