444d2deb8d1a8518d6bbdf747129c18996ee9fdd
fily1212 <fily1212@gmail.com>
2022-03-23 15:07:32 +0100

    Alert

--- esempio-javafx/src/main/java/com/example/esempiojavafx/HelloController.java
+++ esempio-javafx/src/main/java/com/example/esempiojavafx/HelloController.java
@@ -2,6 +2,7 @@ package com.example.esempiojavafx;
 
 import com.example.esempiojavafx.model.Registro;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -21,9 +22,16 @@ public class HelloController {
 
     @FXML
     protected void onAddPersonaButtonClick() {
-        Registro.getInstance().addPersona(nomeTextField.getText());
-        personeLabel.setText(Registro.getInstance().getNomiPersone());
+        if(nomeTextField.getText().equals("")){
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setTitle("Errore");
+            a.setContentText("Non hai inserito il nome");
+            a.show();
+        }else{
+            Registro.getInstance().addPersona(nomeTextField.getText());
+            personeLabel.setText(Registro.getInstance().getNomiPersone());
 //        welcomeText.setText("Welcome to JavaFX Application!");
+        }
     }
 
 
