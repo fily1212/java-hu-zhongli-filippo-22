934cfe320f94b25ece393a58fc711604436927ec
fily1212 <fily1212@gmail.com>
2022-03-24 13:26:49 +0100

    db

---
+++ progetto-intellij/src/com/engim/lezione12/db/JDBCExample.java
@@ -0,0 +1,32 @@
+package com.engim.lezione12.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExample {
+
+    /*SELECT */
+    static final String DB_URL = "jdbc:mysql://localhost:3306/java";
+    static final String USER = "java";
+    static final String PASS = "java";
+    static final String QUERY = "SELECT nome FROM liste";
+
+    public static void main(String[] args) {
+        // Open a connection
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY);
+
+            while(rs.next()){
+                //Display values
+               System.out.println(rs.getString("nome"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
