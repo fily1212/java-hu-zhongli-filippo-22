ec148e6dc89f26d40225398924a535c694ac3143
fily1212 <fily1212@gmail.com>
2022-03-30 11:21:14 +0200

    db spring

---
+++ spring-example/src/main/java/org/engim/springexample/controller/SalutoController.java
@@ -0,0 +1,19 @@
+package org.engim.springexample.controller;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.engim.springexample.model.Saluto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SalutoController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Saluto greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Saluto(counter.incrementAndGet(), String.format(template, name));
+    }
+}
