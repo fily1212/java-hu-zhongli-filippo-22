ea574d0c4adb845020c79bb3cb47fc8ffbafbf8c
fily1212 <fily1212@gmail.com>
2022-04-05 09:25:52 +0200

    esame

---
+++ esempio-esame-prova/src/main/java/com/example/esempioesameprova/EsempioEsameProvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.esempioesameprova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsempioEsameProvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsempioEsameProvaApplication.class, args);
+    }
+
+}
---
+++ esempio-esame-prova/src/test/java/com/example/esempioesameprova/EsempioEsameProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esempioesameprova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsempioEsameProvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
